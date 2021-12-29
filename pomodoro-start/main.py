95ea77007edeeac94f2dd8399f86e27e682c81c5
kenshi1202 <kenshi1202@gmail.com>
2021-12-29 22:50:20 +0900

    section 28-258

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,34 +11,42 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(WORK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count):
+    count_min = str(math.floor(count / 60)).zfill(2)
+    count_sec = str(count % 60).zfill(2)
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100 ,pady=30, bg=YELLOW)
+window.config(padx=100, pady=30, bg=YELLOW)
 
-label_title = Label(text="Timer",fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+label_title = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
 label_title.grid(row=0, column=1)
 
 canvas = Canvas(width=250, height=270, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(125, 150, image=tomato_img)
-canvas.create_text(125, 170, text="00:00",fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(125, 170, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-button_start = Button(text="Start")
+button_start = Button(text="Start", command=start_timer)
 button_start.grid(row=2, column=0)
 
 button_end = Button(text="Reset")
 button_end.grid(row=2, column=2)
 
-label_checkmark = Label(text="✓",fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, "bold"))
+label_checkmark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, "bold"))
 label_checkmark.grid(row=3, column=1)
 
 window.mainloop()
