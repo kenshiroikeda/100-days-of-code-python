db407f8ad63a5d3c69a19c42cf90f89a218afd52
kenshi1202 <kenshi1202@gmail.com>
2022-01-05 10:04:30 +0900

    lesson 290

--- day32/main.py
+++ day32/main.py
@@ -1,10 +1,19 @@
-import smtplib
+# import smtplib
+#
+# my_email = "mail@mail.com"
+#
+# with smtplib.SMTP("smtp.mail.com", port=111) as connection :
+#     connection.starttls()
+#     connection.login(user=my_email, password="password")
+#     connection.sendmail(from_addr=my_email, to_addrs=my_email, msg="Hello")
+#
 
-my_email = "mail@mail.com"
+import datetime as dt
 
-with smtplib.SMTP("smtp.mail.com", port=111) as connection :
-    connection.starttls()
-    connection.login(user=my_email, password="password")
-    connection.sendmail(from_addr=my_email, to_addrs=my_email, msg="Hello")
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
 
-
+day_of_birth = dt.datetime(year=1986, month=12, day=2)
+print(day_of_birth)
