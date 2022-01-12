484763b47c5bbc9a397d2c1c31ea789225a46675
kenshi1202 <kenshi1202@gmail.com>
2022-01-12 09:54:25 +0900

    on the way of lesson 306

--- day34/quizzler-app-start/ui.py
+++ day34/quizzler-app-start/ui.py
@@ -1,2 +1,12 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+        Canvas(width=250, height=300, bg="WHITE", highlightthickness=0)
+        self.window.mainloop()
