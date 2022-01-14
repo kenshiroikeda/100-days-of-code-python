465cf50e5e08e4200db6973ebc769d365401fe7c
kenshi1202 <kenshi1202@gmail.com>
2022-01-14 22:28:29 +0900

    lesson 313

--- day35/main.py
+++ day35/main.py
@@ -1,1 +1,16 @@
-API_KEY = "b03e013b6fb012e070315f3dedafa403"
+import requests
+
+API_KEY = "b2c7a4738c612e61783a2ade1c569445"
+LAT = 35.694706
+LON = 139.982620
+
+param = {
+    "lat": LAT,
+    "lon": LON,
+    "exclude": "current,minutely,daily,alerts",
+    "appid": API_KEY
+}
+
+res = requests.get(url="https://api.openweathermap.org/data/2.5/onecall",params=param)
+res.raise_for_status()
+print(res.json()["hourly"])
