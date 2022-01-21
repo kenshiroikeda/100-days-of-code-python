447f33d49bdc031cc6410b9df2b062a7e81c26eb
kenshi1202 <kenshi1202@gmail.com>
2022-01-21 23:02:32 +0900

    complete day38

--- day38/main.py
+++ day38/main.py
@@ -1,10 +1,11 @@
+import datetime
 import os
 import requests
 
 API_KEY = os.getenv("NUT_APIKEY")
 APP_ID = os.getenv("NUT_APP_ID")
 POST_URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
-
+POST_SHEETY_URL = os.getenv("POST_SHEETY_URL")
 input_txt = input("Tell me which exercise you did")
 
 header_param = {
@@ -23,6 +24,28 @@ request_param = {
 
 res = requests.post(url=POST_URL, json=request_param, headers=header_param)
 res.raise_for_status()
-print(res.text)
 
+sheety_header_param = {
+    "Authorization": os.getenv("SHEETY_AUTH"),
+    "Content-Type":  "application/json"
+}
+
+now = datetime.datetime.now()
+now_date = now.strftime("%d/%m/%Y")
+now_time = now.strftime("%I:%M:%S %p")
+
+
+for data in res.json()["exercises"]:
+    sheety_param = {
+        "workout": {
+            "date": now_date,
+            "time": now_time,
+            "exercise": data["name"],
+            "duration": data["duration_min"],
+            "calories": data["nf_calories"]
+        }
+    }
+
+    res_sheety = requests.post(url=POST_SHEETY_URL, json=sheety_param, headers=sheety_header_param)
+    res_sheety.raise_for_status()
 
