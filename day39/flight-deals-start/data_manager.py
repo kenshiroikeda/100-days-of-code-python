c86c4e263e2c0abd76f26e3009eaf60f671bc3ab
kenshi1202 <kenshi1202@gmail.com>
2022-01-24 23:17:07 +0900

    day39 get sheety data

--- day39/flight-deals-start/data_manager.py
+++ day39/flight-deals-start/data_manager.py
@@ -1,3 +1,15 @@
+import os
+import requests
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    END_POINT = os.getenv("SHEETY_END_POINT")
+
+    header = {
+        "Authorization": os.getenv("SHEETY_APIKEY")
+    }
+
+    def load_sheet_data(self):
+        res = requests.get(url=self.END_POINT, headers=self.header)
+        res.raise_for_status()
+        print(res.json())
--- day39/flight-deals-start/main.py
+++ day39/flight-deals-start/main.py
@@ -1,5 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from flight_search import FlightSearch
+from data_manager import DataManager
 
-fs = FlightSearch()
-fs.search_flight()
+# fs = FlightSearch()
+# fs.search_flight()
+dm = DataManager()
+dm.load_sheet_data()
