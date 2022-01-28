27974da05021438891c7ab3947ad748b0b14cb39
kenshi1202 <kenshi1202@gmail.com>
2022-01-28 13:49:14 +0900

    complete day 39

--- day39/flight-deals-start/main.py
+++ day39/flight-deals-start/main.py
@@ -1,12 +1,15 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from flight_search import FlightSearch
 from data_manager import DataManager
+from notification_manager import NotificationManager
 
 ORIGIN_CITY_IATA = "LON"
 
 fs = FlightSearch()
 dm = DataManager()
+nm = NotificationManager()
 loaded_sheet_data = dm.load_sheet_data()
+
 
 iata_list = fs.fill_iata_code(loaded_sheet_data)
 dm.update_sheet_data(iata_list)
@@ -16,3 +19,4 @@ for destination in iata_list:
         ORIGIN_CITY_IATA,
         destination["iataCode"]
     )
+
--- day39/flight-deals-start/notification_manager.py
+++ day39/flight-deals-start/notification_manager.py
@@ -1,3 +1,21 @@
+import os
+from twilio.rest import Client
+from flight_data import FlightData
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    def sendFlightDetailMessage(self, flight_detail: FlightData):
+        content = f"Low Price Alart!Only{flight_detail.price} to fly from {flight_detail.departure_city}-" \
+                  f"{flight_detail.departure_airport_code} to {flight_detail.destination_city}-" \
+                  f"{flight_detail.destination_airport_code}, from {flight_detail.out_date} to from " \
+                  f"{flight_detail.return_date}"
+        message = self.client.messages.create(
+            body=content,
+            from_='+11',
+            to='+11'
+        )
