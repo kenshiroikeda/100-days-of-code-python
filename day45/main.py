bbe9a966992ecddd51b5493521ec830d924878e6
kenshi1202 <kenshi1202@gmail.com>
2022-02-12 23:29:09 +0900

    lesson 401

--- day45/main.py
+++ day45/main.py
@@ -1,10 +1,23 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", encoding="utf-8") as html:
-    contents = html.read()
+res = requests.get("https://news.ycombinator.com/")
+soup = BeautifulSoup(res.text, "html.parser")
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# print(soup.title.string)
+article_tags = soup.find_all(name="a", class_="titlelink")
+article_texts = []
+article_links = []
 
-print(soup.find_all(name="a"))
+for tag in article_tags:
+    article_txt = tag.getText()
+    article_texts.append(article_txt)
+    article_link = tag.get("href")
+    article_links.append(article_link)
+
+article_upvotes = [int(score.getText().split(" ")[0]) for score in soup.find_all(name="span", class_="score")]
+
+max_num = max(article_upvotes)
+max_index = article_upvotes.index(max_num)
+
+print(article_texts[max_index])
+print(article_links[max_index])
