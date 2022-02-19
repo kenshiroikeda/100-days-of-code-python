b3b33c75007f5f2416890c47789f27ba1487f207
kenshi1202 <kenshi1202@gmail.com>
2022-02-19 10:57:43 +0900

    complete day48

--- day48/interaction.py
+++ day48/interaction.py
@@ -4,21 +4,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 DRIVER_PATH = "C:\chromedriver\chromedriver.exe"
-URL="https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"
+URL="http://secure-retreat-92358.herokuapp.com/"
 
 chrome_service = fs.Service(executable_path=DRIVER_PATH)
 driver = webdriver.Chrome(service=chrome_service)
 driver.get(URL)
-element = driver.find_element(by=By.CSS_SELECTOR, value="#number b a")
-element.click()
 
-all_portals = driver.find_element(by=By.LINK_TEXT, value="総ページ数")
-all_portals.click()
+f_name = driver.find_element(by=By.NAME, value="fName")
+f_name.send_keys("Ken")
 
-search_box = driver.find_element(by=By.NAME, value="search")
-search_box.send_keys("Python")
-search_box.send_keys(Keys.ENTER)
+l_name = driver.find_element(by=By.NAME, value="lName")
+l_name.send_keys("Ike")
 
-# search_button = driver.find_element(by=By.ID, value="searchButton")
-# search_button.click()
+email = driver.find_element(by=By.NAME, value="email")
+email.send_keys("kenike@mail.com")
 
+btn_signup = driver.find_element(by=By.TAG_NAME, value="button")
+btn_signup.click()
