1bbf4c9bc32a13f6a24847644d9cde127a862639
kenshi1202 <kenshi1202@gmail.com>
2022-02-23 11:05:50 +0900

    complete lesson 45

--- day55/hello.py
+++ day55/hello.py
@@ -3,9 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>This is a paragraph</p>' \
+           '<img src="https://images.pexels.com/photos/479009/pexels-photo-479009.jpeg?cs=srgb&dl=pexels-danielle-daniel-479009.jpg&fm=jpg" width="200">'
 
 
 @app.route('/bye')
 def day_bye():
     return 'Bye'
+
+
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello, {name}, you are {number} years old!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
