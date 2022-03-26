58e121e963e4da049db9409924fdce264709e21b
kenshi1202 <kenshi1202@gmail.com>
2022-03-26 18:36:08 +0900

    complete lesson 466

--- day57-jinja/main.py
+++ day57-jinja/main.py
@@ -1,11 +1,11 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", current_year=datetime.datetime.now().year)
 
 
 if __name__ == "__main__":
